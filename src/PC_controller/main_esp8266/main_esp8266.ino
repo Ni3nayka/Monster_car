bea33e0bb7b5f918ee4ad6b0242aeea1ff7eb94e
Egor Bakay <egor_bakay@inbox.ru>
2023-05-14 23:12:08 +0300

    add code from AVOCADO project

--- src/PC_controller/main_esp8266/main_esp8266.ino
+++ src/PC_controller/main_esp8266/main_esp8266.ino
@@ -7,21 +7,30 @@
    modify: May 2023
 */
 
-#include "gamepad.h"
+#include "wifi-config.h"
+//#define WIFI_NAME "test_name"
+//#define WIFI_PASSWORD "test_pass"
+//#define SERVER_IP "192.168.1.82"
+//#define SERVER_PORT 1234
+#define ENABLE_AVOCADO_ESP_WIFI_GAMEPAD
+#include <AVOCADO_esp.h>
+AVOCADO_esp esp;
+
 #include "motor.h"
 
 void setup(){
   Serial.begin(9600);
   MotorShield.setup();
+  esp.setup();
 //  gamepad.setup();
 //  setup_motor();
 }
 
 void loop(){
-//  gamepad.update();
-//  motor(LEFT,  gamepad.joystick(PSS_LY));
-//  motor(RIGHT, gamepad.joystick(PSS_RY));
-//  gamepad.vibration = gamepad.button(PSB_BLUE);
+  test_gamepad();
+}
+
+void test_motor() {
   MotorShield.motor_left.run(100);
   for (int i = 0; i<100; i++) {
     MotorShield.motors(i,0);
@@ -50,4 +59,32 @@ void loop(){
     delay(5);
   }
   MotorShield.motors(0,0);
+}
+
+void test_gamepad() {
+  
+  esp.update();
+  
+  // button
+  for (int i = 0; i<GAMEPAD_BUTTON_QUANTITY; i++) {
+    Serial.print(esp.gamepad_button[i]); // bool
+  }
+
+  Serial.print(" <> ");
+  
+  // allow
+  for (int i = 0; i<GAMEPAD_ARROW_QUANTITY; i++) {
+    Serial.print(esp.gamepad_arrow[i]); // int
+    Serial.print(" ");
+  }
+  
+  Serial.print(" <> ");
+  
+  // joystick
+  for (int i = 0; i<GAMEPAD_JOYSTICK_QUANTITY; i++) {
+    Serial.print(esp.gamepad_joystick[i]); // int
+    Serial.print(" ");
+  }
+    
+  Serial.println();
 }
