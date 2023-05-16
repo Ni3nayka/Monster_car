44eded0b43b589544fbc31dcc0316eb683cdcd9a
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-05-16 13:29:35 +0300

    +

--- src/PC_controller/main_esp8266/main_esp8266.ino
+++ src/PC_controller/main_esp8266/main_esp8266.ino
@@ -22,69 +22,10 @@ void setup(){
   Serial.begin(9600);
   MotorShield.setup();
   esp.setup();
-//  gamepad.setup();
-//  setup_motor();
 }
 
 void loop(){
-  test_gamepad();
-}
-
-void test_motor() {
-  MotorShield.motor_left.run(100);
-  for (int i = 0; i<100; i++) {
-    MotorShield.motors(i,0);
-    delay(5);
-  }
-  for (int i = 100; i>-100; i--) {
-    MotorShield.motors(i,0);
-    delay(5);
-  }
-  for (int i = -100; i<0; i++) {
-    MotorShield.motors(i,0);
-    delay(5);
-  }
-  MotorShield.motors(0,0);
-  
-  for (int i = 0; i<100; i++) {
-    MotorShield.motors(0,i);
-    delay(5);
-  }
-  for (int i = 100; i>-100; i--) {
-    MotorShield.motors(0,i);
-    delay(5);
-  }
-  for (int i = -100; i<0; i++) {
-    MotorShield.motors(0,i);
-    delay(5);
-  }
-  MotorShield.motors(0,0);
-}
-
-void test_gamepad() {
-  
+  //test_gamepad();
   esp.update();
-  
-  // button
-  for (int i = 0; i<GAMEPAD_BUTTON_QUANTITY; i++) {
-    Serial.print(esp.gamepad_button[i]); // bool
-  }
-
-  Serial.print(" <> ");
-  
-  // allow
-  for (int i = 0; i<GAMEPAD_ARROW_QUANTITY; i++) {
-    Serial.print(esp.gamepad_arrow[i]); // int
-    Serial.print(" ");
-  }
-  
-  Serial.print(" <> ");
-  
-  // joystick
-  for (int i = 0; i<GAMEPAD_JOYSTICK_QUANTITY; i++) {
-    Serial.print(esp.gamepad_joystick[i]); // int
-    Serial.print(" ");
-  }
-    
-  Serial.println();
+  MotorShield.motors(-esp.gamepad_joystick[1],esp.gamepad_joystick[2]);
 }
