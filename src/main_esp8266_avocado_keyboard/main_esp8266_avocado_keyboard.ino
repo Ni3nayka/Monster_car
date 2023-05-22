a2d5aef1fcdf0f4472fd698093472a4d38081436
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-05-22 13:10:24 +0300

    update rulies

--- src/main_esp8266_avocado_keyboard/main_esp8266_avocado_keyboard.ino
+++ src/main_esp8266_avocado_keyboard/main_esp8266_avocado_keyboard.ino
@@ -19,7 +19,7 @@ AVOCADO_esp esp;
 #include "motor.h"
 
 // debug
-#define DEBUG_SERIAL
+//#define DEBUG_SERIAL
 #define DEBUG_WIFISERIAL
 
 // motor
@@ -32,22 +32,27 @@ AVOCADO_esp esp;
 #define MOTOR_LEFT_BUTTON_OFF "-65"
 #define MOTOR_RIGHT_BUTTON_OFF "-68"
 int forward_vector = 0, left_vector = 0;
-#define FORWARD_VECTOR_K 90
-#define LEFT_VECTOR_K 100
+//#define FORWARD_VECTOR_K 90
+//#define LEFT_VECTOR_K 100
+#define LEFT_VECTOR_K 0.2
 
 // update max speed
-#define UP_SPEED_BUTTON "+39"
-#define DOWN_SPEED_BUTTON "+37"
+#define UP_SPEED_BUTTON "+69" //"+39"
+#define DOWN_SPEED_BUTTON "+81" //"+37"
 #define MOTOR_SPEED_UPDATE_D 10
-int max_motor_speed = 40;
+#define UP_LEFT_SPEED_BUTTON "+67"
+#define DOWN_LEFT_SPEED_BUTTON "+90"
+#define MOTOR_LEFT_SPEED_UPDATE_D 5
+int max_motor_speed = 30;
+int max_left_motor_speed = 20;
 
 // servo
 #define SERVO_UP_BUTTON "+38"
 #define SERVO_DOWN_BUTTON "+40"
 #define SERVO_UPDATE_D 2
 #define SERVO_PIN D2
-#define SERVO_MAX_ANGLE 160
-#define SERVO_MIN_ANGLE 35
+#define SERVO_MAX_ANGLE 150
+#define SERVO_MIN_ANGLE 20
 int servo_angle = 70;
 
 void setup(){
@@ -78,8 +83,8 @@ void robot_main() {
     String input_data = esp.read();
     // servo
     if (input_data==SERVO_UP_BUTTON || input_data==SERVO_DOWN_BUTTON) {
-      if (input_data==SERVO_UP_BUTTON) servo_angle+=SERVO_UPDATE_D;
-      else servo_angle-=SERVO_UPDATE_D;
+      if (input_data==SERVO_UP_BUTTON) servo_angle-=SERVO_UPDATE_D;
+      else servo_angle+=SERVO_UPDATE_D;
       servo_angle = constrain(servo_angle,SERVO_MIN_ANGLE,SERVO_MAX_ANGLE);
       #ifdef DEBUG_SERIAL
       Serial.println("servo_angle: " + String(servo_angle));
@@ -100,6 +105,17 @@ void robot_main() {
       esp.print("update max speed: "+String(max_motor_speed));
       #endif
     }
+    if (input_data==UP_LEFT_SPEED_BUTTON || input_data==DOWN_LEFT_SPEED_BUTTON) {
+      if (input_data==UP_LEFT_SPEED_BUTTON) max_left_motor_speed+=MOTOR_LEFT_SPEED_UPDATE_D;
+      else max_left_motor_speed-=MOTOR_LEFT_SPEED_UPDATE_D;
+      max_left_motor_speed = constrain(max_left_motor_speed,0,100);
+      #ifdef DEBUG_SERIAL
+      Serial.println("update max left speed: "+String(max_left_motor_speed));
+      #endif
+      #ifdef DEBUG_WIFISERIAL
+      esp.print("update max left speed: "+String(max_left_motor_speed));
+      #endif
+    }
     // motors
     if (input_data==MOTOR_FORWARD_BUTTON) forward_vector = 1;
     if (input_data==MOTOR_BACKWARD_BUTTON) forward_vector = -1;
@@ -108,8 +124,24 @@ void robot_main() {
     if (input_data==MOTOR_RIGHT_BUTTON) left_vector = -1;
     if (input_data==MOTOR_LEFT_BUTTON_OFF || input_data==MOTOR_RIGHT_BUTTON_OFF) left_vector = 0;
     // run motors
-    int left_speed = constrain(forward_vector*FORWARD_VECTOR_K-left_vector*LEFT_VECTOR_K,-max_motor_speed,max_motor_speed);
-    int right_speed = constrain(forward_vector*FORWARD_VECTOR_K+left_vector*LEFT_VECTOR_K,-max_motor_speed,max_motor_speed);
+    int left_speed = 0;
+    int right_speed = 0;
+    if (forward_vector==0) {
+      right_speed = left_vector*max_left_motor_speed;
+      left_speed = -right_speed;
+    }
+    else { // if (left_vector==0) {
+      left_speed = forward_vector*max_motor_speed;
+      right_speed = left_speed;
+      left_speed = left_vector==1?left_speed*LEFT_VECTOR_K:left_speed;
+      right_speed = left_vector==-1?right_speed*LEFT_VECTOR_K:right_speed;
+    }
+//    else {
+//      left_speed = max_left_motor_speed==1?forward_vector*max_motor_speed*0.5
+//      right_speed =
+////      left_speed = constrain(forward_vector*FORWARD_VECTOR_K-left_vector*LEFT_VECTOR_K,-max_motor_speed,max_motor_speed);
+////      right_speed = constrain(forward_vector*FORWARD_VECTOR_K+left_vector*LEFT_VECTOR_K,-max_motor_speed,max_motor_speed);  
+//    }
     #ifdef DEBUG_SERIAL
     Serial.println("motor_speed: " + String(left_speed) + " " + String(right_speed));
     #endif
