9e29f9ad7fe8602c108a2ce4d4c301d2141e8f71
Egor Bakay <egor_bakay@inbox.ru>
2023-06-09 22:11:42 +0300

    fix const and start new servo fun

--- src/main_esp8266_avocado_keyboard/main_esp8266_avocado_keyboard.ino
+++ src/main_esp8266_avocado_keyboard/main_esp8266_avocado_keyboard.ino
@@ -4,7 +4,7 @@
 
    author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
    write:  May 2022
-   modify: May 2023
+   modify: June 2023
 */
 
 #include "wifi-config.h"
@@ -23,6 +23,7 @@ AVOCADO_esp esp;
 // debug
 //#define DEBUG_SERIAL
 #define DEBUG_WIFISERIAL
+//#define MOTOR_EN
 
 // motor
 #define MOTOR_FORWARD_BUTTON "+87"
@@ -49,35 +50,48 @@ int max_left_motor_speed = 20;
 // servo
 #include "my_servo.h"
 MyServo servo_camera;
+#define SERVO_CAMERA_UP_BUTTON "+38"
+#define SERVO_CAMERA_DOWN_BUTTON "+40"
+#define SERVO_CAMERA_UPDATE_D 2
+#define SERVO_CAMERA_PIN D2
+#define SERVO_CAMERA_MAX_ANGLE 270
+#define SERVO_CAMERA_MIN_ANGLE 86
+
+#define SERVO_UPDATE_D 2
+
 MyServo servo_left_arm;
 MyServo servo_right_arm;
-#define SERVO_UP_BUTTON "+38"
-#define SERVO_DOWN_BUTTON "+40"
-#define SERVO_UPDATE_D 2
-#define SERVO_PIN D1
-#define SERVO_MAX_ANGLE 150
-#define SERVO_MIN_ANGLE 20
-
 #define SERVO_LEFT_ARM_PIN D4
 #define SERVO_RIGHT_ARM_PIN D3
-#define SERVO_ARM_UP_BUTTON "+104"
-#define SERVO_ARM_DOWN_BUTTON "+98"
+#define SERVO_ARM_UP_BUTTON "+36"
+#define SERVO_ARM_DOWN_BUTTON "+33"
 #define SERVO_TRANSLATE_LEFT_TO_RIGHT_ANGLE(a) (275-a) // (265-a)
+#define SERVO_MAX_LEFT_ANGLE 180
 #define SERVO_MIN_LEFT_ANGLE 5
+
+MyServo servo_tail;
+#define SERVO_TAIL_PIN D1
+#define SERVO_TAIL_UP_BUTTON "+34"
+#define SERVO_TAIL_DOWN_BUTTON "+35"
+#define SERVO_MAX_TAIL_ANGLE 180
+#define SERVO_MIN_TAIL_ANGLE 0
+
+
 //int servo_angle = 70;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(SERVO_PIN,OUTPUT);
+  //pinMode(SERVO_CAMERA_PIN,OUTPUT);
   MotorShield.setup();
   esp.setup();
-  //servo_camera.attach(SERVO_PIN,350,2600,0,270);
-  servo_camera.attach(SERVO_PIN,500,2500,0,180);
+  servo_camera.attach(SERVO_CAMERA_PIN,350,2600,0,270);
   servo_left_arm.attach(SERVO_LEFT_ARM_PIN,350,2600,0,270);
   servo_right_arm.attach(SERVO_RIGHT_ARM_PIN,350,2600,0,270);
+  servo_tail.attach(SERVO_TAIL_PIN,500,2500,0,180);
   servo_camera.write(150);
   servo_left_arm.write(SERVO_MIN_LEFT_ANGLE);
   servo_right_arm.write(SERVO_TRANSLATE_LEFT_TO_RIGHT_ANGLE(servo_left_arm.get_angle()));
+  servo_tail.write(90);
 }
 
 void loop(){
@@ -99,54 +113,46 @@ void robot_main() {
   esp.update();
   if (esp.available()) {
     String input_data = esp.read();
-    //esp.print(input_data);
-    // servo
-    if (input_data==SERVO_UP_BUTTON || input_data==SERVO_DOWN_BUTTON) {
-      if (input_data==SERVO_UP_BUTTON) servo_camera.write_on(-SERVO_UPDATE_D);
-      else servo_camera.write_on(SERVO_UPDATE_D);
+    esp.print(input_data);
+    // servo camera
+    if (input_data==SERVO_CAMERA_UP_BUTTON || input_data==SERVO_CAMERA_DOWN_BUTTON) {
+      if (input_data==SERVO_CAMERA_UP_BUTTON) servo_camera.write_on(-SERVO_CAMERA_UPDATE_D);
+      else servo_camera.write_on(SERVO_CAMERA_UPDATE_D);
+      if (servo_camera.get_angle()<SERVO_CAMERA_MIN_ANGLE) servo_camera.write(SERVO_CAMERA_MIN_ANGLE);
+      if (servo_camera.get_angle()>SERVO_CAMERA_MAX_ANGLE) servo_camera.write(SERVO_CAMERA_MAX_ANGLE);
       //servo_angle  = constrain(servo_angle,SERVO_MIN_ANGLE,SERVO_MAX_ANGLE);
-      #ifdef DEBUG_SERIAL
-      Serial.println("camera_angle: " + String(servo_camera.get_angle()));
-      #endif
-      #ifdef DEBUG_WIFISERIAL
-      esp.print("camera_angle: " + String(servo_camera.get_angle()));
-      #endif
+      print_data("camera_angle: " + String(servo_camera.get_angle()));
     }
     // servo arm
     if (input_data==SERVO_ARM_UP_BUTTON || input_data==SERVO_ARM_DOWN_BUTTON) {
       if (input_data==SERVO_ARM_UP_BUTTON) servo_left_arm.write_on(-SERVO_UPDATE_D);
       else  servo_left_arm.write_on(SERVO_UPDATE_D);
       if (servo_camera.get_angle()<SERVO_MIN_LEFT_ANGLE) servo_camera.write(SERVO_MIN_LEFT_ANGLE);
+      if (servo_camera.get_angle()>SERVO_MAX_LEFT_ANGLE) servo_camera.write(SERVO_MAX_LEFT_ANGLE);
       servo_right_arm.write(SERVO_TRANSLATE_LEFT_TO_RIGHT_ANGLE(servo_left_arm.get_angle()));
-//      #ifdef DEBUG_SERIAL
-//      Serial.println("arm_angle: " + String(servo_left_arm.get_angle()) + " " + String(servo_right_arm.get_angle()));
-//      #endif
-//      #ifdef DEBUG_WIFISERIAL
-//      esp.print("arm_angle: " + String(servo_left_arm.get_angle()) + " " + String(servo_right_arm.get_angle()));
-//      #endif
+      print_data("arm_angle: " + String(servo_left_arm.get_angle()) + " " + String(servo_right_arm.get_angle()));
+    }
+    // servo tail
+    if (input_data==SERVO_TAIL_UP_BUTTON || input_data==SERVO_TAIL_DOWN_BUTTON) {
+      if (input_data==SERVO_ARM_UP_BUTTON) servo_left_arm.write_on(-SERVO_UPDATE_D);
+      else  servo_left_arm.write_on(SERVO_UPDATE_D);
+      if (servo_camera.get_angle()<SERVO_MIN_LEFT_ANGLE) servo_camera.write(SERVO_MIN_LEFT_ANGLE);
+      if (servo_camera.get_angle()>SERVO_MAX_LEFT_ANGLE) servo_camera.write(SERVO_MAX_LEFT_ANGLE);
+      servo_right_arm.write(SERVO_TRANSLATE_LEFT_TO_RIGHT_ANGLE(servo_left_arm.get_angle()));
+      print_data("arm_angle: " + String(servo_left_arm.get_angle()) + " " + String(servo_right_arm.get_angle()));
     }
     // max_speed
     if (input_data==UP_SPEED_BUTTON || input_data==DOWN_SPEED_BUTTON) {
       if (input_data==UP_SPEED_BUTTON) max_motor_speed+=MOTOR_SPEED_UPDATE_D;
       else max_motor_speed-=MOTOR_SPEED_UPDATE_D;
       max_motor_speed = constrain(max_motor_speed,0,100);
-      #ifdef DEBUG_SERIAL
-      Serial.println("update max speed: "+String(max_motor_speed));
-      #endif
-      #ifdef DEBUG_WIFISERIAL
-      esp.print("update max speed: "+String(max_motor_speed));
-      #endif
+      print_data("update max speed: "+String(max_motor_speed));
     }
     if (input_data==UP_LEFT_SPEED_BUTTON || input_data==DOWN_LEFT_SPEED_BUTTON) {
       if (input_data==UP_LEFT_SPEED_BUTTON) max_left_motor_speed+=MOTOR_LEFT_SPEED_UPDATE_D;
       else max_left_motor_speed-=MOTOR_LEFT_SPEED_UPDATE_D;
       max_left_motor_speed = constrain(max_left_motor_speed,0,100);
-      #ifdef DEBUG_SERIAL
-      Serial.println("update max left speed: "+String(max_left_motor_speed));
-      #endif
-      #ifdef DEBUG_WIFISERIAL
-      esp.print("update max left speed: "+String(max_left_motor_speed));
-      #endif
+      print_data("update max left speed: "+String(max_left_motor_speed));
     }
     // motors
     if (input_data==MOTOR_FORWARD_BUTTON) forward_vector = 1;
@@ -171,10 +177,23 @@ void robot_main() {
     #ifdef DEBUG_SERIAL
     Serial.println("motor_speed: " + String(left_speed) + " " + String(right_speed));
     #endif
+    #ifdef MOTOR_EN
     MotorShield.motors(left_speed,right_speed);
+    #else
+    if (left_speed!=0 || right_speed!=0) print_data("WARNING: motor disable!");
+    #endif
   }
   servo_camera.write();
   servo_left_arm.write();
   servo_right_arm.write();
   //write_servo(servo_angle);
+}
+
+void print_data(String a) {
+  #ifdef DEBUG_SERIAL
+  Serial.println(a);
+  #endif
+  #ifdef DEBUG_WIFISERIAL
+  esp.print(a);
+  #endif
 }
--- src/main_esp8266_avocado_keyboard/my_servo.h
+++ src/main_esp8266_avocado_keyboard/my_servo.h
@@ -13,26 +13,36 @@
 #define SERVO_MAX_TIME 2400
 #define SERVO_MIN_ANGLE 0
 #define SERVO_MAX_ANGLE 180
+#define SERVO_NONE -9999
+//#define SERVO_MULTITHREAD // для многопоточности (внешней)
 
 class MyServo {
   public:
-    //int reverse;
-    void attach(int pin, int min_time=SERVO_MIN_TIME, int max_time=SERVO_MAX_TIME, int min_angle=SERVO_MIN_ANGLE, int max_angle=SERVO_MAX_ANGLE) {
+    int pos;
+    void attach(int pin, int min_time=SERVO_MIN_TIME, int max_time=SERVO_MAX_TIME, int min_real_angle=SERVO_MIN_ANGLE, int max_real_angle=SERVO_MAX_ANGLE, int min_angle=SERVO_NONE, int max_angle=SERVO_NONE) {
+      // min_real_angle - потенциально возможные углы сервы
+      // min_angle - насколько ограничить из предыдущего диапазона
       MyServo::pin = pin;
       pinMode(MyServo::pin,OUTPUT);
       MyServo::min_time = min_time;
       MyServo::max_time = max_time;
-      MyServo::min_angle = min_angle;
-      MyServo::max_angle = max_angle;
+      MyServo::min_real_angle = min_real_angle;
+      MyServo::max_real_angle = max_real_angle;
+      if (min_angle==SERVO_NONE) MyServo::min_angle = min_real_angle;
+      else MyServo::min_angle = min_angle;
+      if (max_angle==SERVO_NONE) MyServo::max_angle = max_real_angle;
+      else MyServo::max_angle = max_angle;
     }
-    void write(int angle=-9999) {
-      if (angle!=-9999) MyServo::angle = constrain(angle,min_angle,max_angle);
-      int pos = map(MyServo::angle,min_angle,max_angle,min_time,max_time);
+    void write(int angle=SERVO_NONE) { // сделать многопоточно
+      if (angle!=SERVO_NONE) MyServo::angle = constrain(angle,MyServo::min_angle,MyServo::max_angle);
+      MyServo::pos = map(MyServo::angle,MyServo::min_real_angle,MyServo::max_real_angle,MyServo::min_time,MyServo::max_time);
+      #ifndef SERVO_MULTITHREAD
       digitalWrite(MyServo::pin, HIGH);
-      delayMicroseconds(pos);
+      delayMicroseconds(MyServo::pos);
       digitalWrite(MyServo::pin, LOW);
-      delayMicroseconds(20000 - pos);
+      delayMicroseconds(20000 - MyServo::pos);
       delay(1);
+      #endif
     }
     void write_on(int d_angle) {
       MyServo::write(MyServo::angle+d_angle);
@@ -41,5 +51,5 @@ class MyServo {
       return MyServo::angle;
     }
   private:
-    int pin,min_time,max_time,min_angle,max_angle,angle;
+    int pin,min_time,max_time,min_real_angle,max_real_angle,min_angle,max_angle,angle;
 };
