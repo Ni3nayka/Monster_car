604abd82e0c5ec9534260b1628dd27f80256dc8e
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-03-14 15:38:41 +0300

    Update radio_pult.ino

--- src/radio_pult/radio_pult.ino
+++ src/radio_pult/radio_pult.ino
@@ -1,3 +1,15 @@
+/*
+   code write for project:
+   https://github.com/Ni3nayka/Monster_car
+
+   author: Egor Bakay <egor_bakay@inbox.ru> Ni3nayka
+   write:  Match 2024
+   modify: Match 2024
+
+   links:
+   https://lesson.iarduino.ru/page/urok-26-6-soedinyaem-dve-arduino-po-radiokanalu-433-mgc/
+*/
+
 #define RADIO_PIN 2
 #include <iarduino_RF433_Transmitter.h>
 iarduino_RF433_Transmitter radio(RADIO_PIN);
