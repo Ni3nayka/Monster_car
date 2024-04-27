c8d547528fd758a085a2e24b03041675ec045b0b
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-04-27 20:30:24 +0300

    +

--- src/main_FlySky/main_FlySky.ino
+++ src/main_FlySky/main_FlySky.ino
@@ -15,22 +15,25 @@
 
 void setup(){
   // Serial.begin(115200);
-  FlySky.begin(Serial); // Serial2
+  // FlySky.begin(Serial); // Serial2
   MotorShield.setup();
+  MotorShield.run(100,100);
+  delay(1000);
+  MotorShield.run();
 }
 
 void loop(){
-  // for (int i = 0; i<14; i++) Serial.print(String(FlySky.readChannel(i)) + " "); Serial.println();
-  int joystick_left_y = FlySky.readChannel(2);
-  int joystick_left_x = FlySky.readChannel(3);
-  int joystick_right_y = FlySky.readChannel(1);
-  int joystick_right_x = FlySky.readChannel(0);
-  int forward = joystick_right_y*EXACT_FORWARD_K + (joystick_left_y+100)/2;
-  int turn = (joystick_left_x + joystick_right_x)*TURN_K;
-  int left_speed = forward + turn;
-  int right_speed = forward - turn;
-  // Serial.println(String(left_speed) + " " + String(right_speed));
-  MotorShield.motors(left_speed, right_speed);
-  delay(10);
+  // // for (int i = 0; i<14; i++) Serial.print(String(FlySky.readChannel(i)) + " "); Serial.println();
+  // int joystick_left_y = FlySky.readChannel(2);
+  // int joystick_left_x = FlySky.readChannel(3);
+  // int joystick_right_y = FlySky.readChannel(1);
+  // int joystick_right_x = FlySky.readChannel(0);
+  // int forward = joystick_right_y*EXACT_FORWARD_K + (joystick_left_y+100)/2;
+  // int turn = (joystick_left_x + joystick_right_x)*TURN_K;
+  // int left_speed = forward + turn;
+  // int right_speed = forward - turn;
+  // // Serial.println(String(left_speed) + " " + String(right_speed));
+  // MotorShield.motors(left_speed, right_speed);
+  // delay(10);
 }
 
--- src/main_FlySky/motor.h
+++ src/main_FlySky/motor.h
@@ -9,29 +9,49 @@
 
 #pragma once
 
-#define MOTOR_DIR_A_1 3
-#define MOTOR_DIR_A_2 5
-#define MOTOR_DIR_B_1 9
-#define MOTOR_DIR_B_2 6
+#define MOTOR_DIR_A_1 14
+#define MOTOR_DIR_A_2 27
+#define MOTOR_DIR_A_PWM 26
+#define MOTOR_DIR_B_1 32
+#define MOTOR_DIR_B_2 33
+#define MOTOR_DIR_B_PWM 25
+
+
+// устанавливаем настройки формирования ШИМ
+#define MOTOR_PWM_HZ 15000 // частота ШИМ
+#define MOTOR_BITS 8 // разрядность (для управления) ШИМ
+#define MOTOR_MAP 2.5 // для перевода скорости из размерности 100 в размерность в соответствии с MOTOR_BITS (8 бит => 255 "ШИМ" ~ 250)
+int global_channel_pwm = 0;
 
 class Motor {
   public:
     //int reverse;
-    void setup(int pin1, int pin2) {
+    void setup(int pin1, int pin2, int pin_pwm) {
       Motor::pin1 = pin1;
       Motor::pin2 = pin2;
+      Motor::pin_pwm = pin_pwm;
       pinMode(Motor::pin1,OUTPUT);
       pinMode(Motor::pin2,OUTPUT);
+      // pinMode(Motor::pin_pwm,OUTPUT);
+      ledcSetup(global_channel_pwm, MOTOR_PWM_HZ, MOTOR_BITS);
+      ledcAttachPin(Motor::pin_pwm, global_channel_pwm);
+      Motor::channel = global_channel_pwm;
+      global_channel_pwm++;
+      // переключаем ШИМы в ноль(чтобы все стояло)
+      Motor::run();
       //reverse = 1;
     }
-    void run(int speed) {
-      speed = map(constrain(speed,-100,100),-100,100,-255,255);
+    void run(int speed=0) {
+      speed = constrain(speed,-100,100)*MOTOR_MAP;
       //speed *= Motor::reverse;
-      analogWrite(Motor::pin1, speed>0?speed:0);
-      analogWrite(Motor::pin2, speed>0?0:-speed);
+      // analogWrite(Motor::pin1, speed>0?speed:0);
+      // analogWrite(Motor::pin2, speed>0?0:-speed);
+      ledcWrite(Motor::channel, abs(speed));
+      digitalWrite(Motor::pin1, speed>0);
+      digitalWrite(Motor::pin2, speed<0);
     }
   private:
-    int pin1, pin2;
+    int pin1, pin2, pin_pwm, channel;
 };
 
 class MyMotorShield {
@@ -39,15 +59,15 @@ class MyMotorShield {
     Motor motor_left;
     Motor motor_right;
     void setup() {
-      MyMotorShield::motor_left.setup(MOTOR_DIR_A_1,MOTOR_DIR_A_2);
-      MyMotorShield::motor_right.setup(MOTOR_DIR_B_1,MOTOR_DIR_B_2);
+      MyMotorShield::motor_left.setup(MOTOR_DIR_A_1,MOTOR_DIR_A_2,MOTOR_DIR_A_PWM);
+      MyMotorShield::motor_right.setup(MOTOR_DIR_B_1,MOTOR_DIR_B_2,MOTOR_DIR_B_PWM);
     }
-    void motors(int speed_left, int speed_right) {
+    void run(int speed_left=0, int speed_right=0) {
       MyMotorShield::motor_left.run(speed_left);
       MyMotorShield::motor_right.run(speed_right);
     }
     void stop_motors() {
-      MyMotorShield::motors(0,0);
+      MyMotorShield::run();
     }
 };
 
