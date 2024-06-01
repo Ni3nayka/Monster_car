aec481dba2e72eef649fbcc48ff6a517397bf920
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-06-01 13:24:17 +0300

    lib for lazer

---
+++ src/test_laser/laser_lib/laser_lib.ino
@@ -0,0 +1,16 @@
+#include "lazer.h"
+
+Lazer lazer_1(0x30,18);
+Lazer lazer_2(0x31,19);
+
+void setup() {
+  Serial.begin(115200);
+  firstSetupLazers();
+  lazer_1.setup();
+  lazer_2.setup();
+}
+
+void loop() {
+   Serial.println(String(lazer_1.get()) + " " + String(lazer_2.get()));
+  delay(100);
+}
