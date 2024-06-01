97e7055ee4157ded71cc41d74a530aa384c860db
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2024-06-01 16:51:25 +0300

    Update motor.h

--- src/main_FlySky/motor.h
+++ src/main_FlySky/motor.h
@@ -18,7 +18,7 @@
 
 
 // устанавливаем настройки формирования ШИМ
-#define MOTOR_PWM_HZ 15000 // частота ШИМ
+#define MOTOR_PWM_HZ 30000 // частота ШИМ
 #define MOTOR_BITS 8 // разрядность (для управления) ШИМ
 #define MOTOR_MAP 2.5 // для перевода скорости из размерности 100 в размерность в соответствии с MOTOR_BITS (8 бит => 255 "ШИМ" ~ 250)
 int global_channel_pwm = 0;
@@ -26,32 +26,27 @@ int global_channel_pwm = 0;
 class Motor {
   public:
     //int reverse;
-    void setup(int pin1, int pin2, int pin_pwm) {
+    void setup(int pin1, int pin2, int pin_en) {
       Motor::pin1 = pin1;
       Motor::pin2 = pin2;
-      Motor::pin_pwm = pin_pwm;
-      pinMode(Motor::pin1,OUTPUT);
-      pinMode(Motor::pin2,OUTPUT);
-      // pinMode(Motor::pin_pwm,OUTPUT);
+      Motor::pin_en = pin_en;
+      pinMode(Motor::pin_en,OUTPUT);
       ledcSetup(global_channel_pwm, MOTOR_PWM_HZ, MOTOR_BITS);
-      ledcAttachPin(Motor::pin_pwm, global_channel_pwm);
+      ledcSetup(global_channel_pwm+1, MOTOR_PWM_HZ, MOTOR_BITS);
+      ledcAttachPin(Motor::pin1, global_channel_pwm);
+      ledcAttachPin(Motor::pin2, global_channel_pwm+1);
       Motor::channel = global_channel_pwm;
-      global_channel_pwm++;
-      // переключаем ШИМы в ноль(чтобы все стояло)
+      global_channel_pwm+=2;
       Motor::run();
-      //reverse = 1;
     }
     void run(int speed=0) {
       speed = constrain(speed,-100,100)*MOTOR_MAP;
-      //speed *= Motor::reverse;
-      // analogWrite(Motor::pin1, speed>0?speed:0);
-      // analogWrite(Motor::pin2, speed>0?0:-speed);
-      ledcWrite(Motor::channel, abs(speed));
-      digitalWrite(Motor::pin1, speed>0);
-      digitalWrite(Motor::pin2, speed<0);
+      digitalWrite(Motor::pin_en,speed!=0);
+      ledcWrite(Motor::channel, constrain(speed,0,255));
+      ledcWrite(Motor::channel+1, constrain(-speed,0,255));
     }
   private:
-    int pin1, pin2, pin_pwm, channel;
+    int pin1, pin2, pin_en, channel;
 };
 
 class MyMotorShield {
