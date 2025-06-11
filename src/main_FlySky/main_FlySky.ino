67151fede5f70d5830a9772520d7f0c9392ea1e3
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2025-06-11 23:54:59 +0300

    Update main_FlySky.ino

--- src/main_FlySky/main_FlySky.ino
+++ src/main_FlySky/main_FlySky.ino
@@ -7,7 +7,7 @@
    modify: June 2025
 */
 
-// #define WIFI_BOOTLOAD
+// #define WIFI_BOOTLOAD // почему-то esp не пашет, уходит в вечный ребут...возможно из-за очерендого обновления ядря
 #ifdef WIFI_BOOTLOAD
 #define ENABLE_AVOCADO_ESP_WIFI_BOOT
 #include "user_data.h"
@@ -24,38 +24,46 @@ unsigned long int main_flysky_time = 0;
 
 
 #include "SoftwareSerial.h"
-SoftwareSerial servoDriver(5, 18); // RX, TX
+SoftwareSerial servoDriver(19, 18); // RX, TX
 #define GY_25_SERIAL servoDriver
 
+#define GY_25_SERIAL servoDriver
 #include "gy-25-parser.h"
 GY25Parser gy25;
+unsigned long int global_timer_for_gyroscope_run = 0;
 
 int global_stick_l_old = -1, global_stick_r_old = -1;
 unsigned long int servo_update_time = 0;
 
-// long int global_integral = 0;
+#define LINE_SENSOR_LEFT 35
+#define LINE_SENSOR_RIGHT 34
+long int global_integral = 0;
 int global_e_old = 0;
 
 void setup(){
-  gy25.setup();
+
+  pinMode(LINE_SENSOR_LEFT,INPUT);
+  pinMode(LINE_SENSOR_RIGHT,INPUT);
 
   Serial.begin(115200);
   MotorShield.setup();
   FlySky.begin(Serial2);
   servoDriver.begin(9600);
+  gy25.setup();
   
   #ifdef WIFI_BOOTLOAD
-  if (digitalRead(ENABLE_BUTTON)==1) {
-    esp.setup();
-  }
-  else {
-    for (int i = 0; i<5; i++) {
-      digitalWrite(ENABLE_LED,1);
-      delay(100);
-      digitalWrite(ENABLE_LED,0);
-      delay(100);
-    }
-  }
+  esp.setup();
+  // if (digitalRead(ENABLE_BUTTON)==1) {
+  //   esp.setup();
+  // }
+  // else {
+  //   for (int i = 0; i<5; i++) {
+  //     digitalWrite(ENABLE_LED,1);
+  //     delay(100);
+  //     digitalWrite(ENABLE_LED,0);
+  //     delay(100);
+  //   }
+  // }
   #endif
   
   MotorShield.run(10, 10);
@@ -64,9 +72,13 @@ void setup(){
 }
 
 void loop() { 
-  mainFlySky();
+  // mainFlySky();
+  gyroscopeRun();
   gy25.parseData();
-  // esp.update(); 
+  
+  #ifdef WIFI_BOOTLOAD
+  esp.update(); 
+  #endif
 }
 
 void mainFlySky() {
@@ -82,6 +94,13 @@ void mainFlySky() {
   int swb = FlySky.readChannel(5)>0;
   int swc = FlySky.readChannel(6); swc = swc==0?0:(swc>0?1:-1);
   int swd = FlySky.readChannel(7)>0;
+
+  // white 1968 2839
+  // black 407 599
+
+  // Serial.print(analogRead(LINE_SENSOR_LEFT)); // L
+  // Serial.print(" ");
+  // Serial.println(analogRead(LINE_SENSOR_RIGHT)); // R
 
   if (swb) {
 
@@ -100,7 +119,7 @@ void mainFlySky() {
     stick_r = map(stick_r,-100,100,0,180);
     if (global_stick_l_old!=stick_l || global_stick_r_old!=stick_r || servo_update_time<millis()) {
       servoDriver.println(String(stick_l)+"1");
-      // Serial.println("[" + String(stick_l) + " " + String(stick_r) + " ]");
+      // Serial.println(String(stick_l)+"1");
       servo_update_time = millis()+1000;
     }
     global_stick_l_old = stick_l;
@@ -110,15 +129,17 @@ void mainFlySky() {
       MotorShield.run(left_speed, right_speed);
     }
     else if (swc==0) { // auto_gyroscope
-      
+      gyroscopeRun();
     }
     else { // auto_black_line
-      float K = 0.2;
-      long int e = (analogRead(35)-2100)*K;
-      int M = 28;
+      float K = 0.1;
+      long int e = (analogRead(LINE_SENSOR_LEFT)-analogRead(LINE_SENSOR_RIGHT))*K;
+      int M = 50; // 28
       // Serial.println(e);
+      // int l = constrain(M+e, -M, M);
+      // int r = constrain(M-e, -M, M);
       int l = constrain(M+e, -M, M);
-      int r = constrain(M-e, -M/2, M);
+      int r = constrain(M-e, -M, M);
       MotorShield.run(l, r);
     }
     
@@ -130,4 +151,28 @@ void mainFlySky() {
   }
 
   delay(10); //   НУЖНО!!!   НУЖНО!!!   НУЖНО!!!   НУЖНО!!!   НУЖНО!!!   НУЖНО!!!   НУЖНО!!!
+}
+
+void gyroscopeRun() {
+  Serial.println(gy25.recieved_data);
+  // if (gy25.recieved_data) {
+  //   gy25.recieved_data = 0;
+  //   // PID
+  //   float K = 0.1;
+  //   long int e = gy25.data[0]*K;
+  //   int M = 50; // 28
+  //   Serial.println(e);
+  //   int l = constrain(M+e, -M, M);
+  //   int r = constrain(M-e, -M, M);
+  //   MotorShield.run(l, r);
+  //   global_timer_for_gyroscope_run = millis() + 500; // вырубиться, если данные перестали поступать
+
+  // }
+  // if (global_timer_for_gyroscope_run<millis()) {
+  //   MotorShield.run();
+  // }
+  // fps
+  delay(500);
+  //gy25.requestData();
+  servoDriver.println("7");
 }
